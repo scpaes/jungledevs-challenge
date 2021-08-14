02336b16c9e41b1db876d14d5350f6d1d24e7c07
scpaes <scp2@cin.ufpe.br>
2021-08-13 22:06:15 -0300

    Update project, and create app

--- setup/urls.py
+++ setup/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('control-panel/', admin.site.urls),
+    path('api/', include('rest_framework.urls'))
+
 ]
