5f6d5407c7b4295800a7c6199cee79f4ffc6a58b
scpaes <scp2@cin.ufpe.br>
2021-08-15 16:52:31 -0300

    update article list view

--- challengenews/views/articles.py
+++ challengenews/views/articles.py
@@ -1,4 +1,4 @@
-from challengenews.serializers.articles import ArticleSerializerAnonymousUser
+from challengenews.serializers.articles import ArticleListSerializer, ArticleSerializerAnonymousUser
 from rest_framework import serializers, viewsets, filters
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.generics import get_object_or_404
@@ -22,23 +22,25 @@ class ArticleListView(viewsets.ViewSet):
         ViewSet for listing or retrieving articles.
     """
     permission_classes = [AllowAny]
+    queryset = Articles.objects.all()
+
     search_fields = ['author__name', 'title', 'slug']
     filterset_fields = ['category', 'slug']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
 
     def get_serializer_class(self, request):
+
         if request.user.is_authenticated:
             return ArticleSerializer
         return ArticleSerializerAnonymousUser
 
     def list(self, request):
-        queryset = Articles.objects.all()
         # serializer = ArticleSerializer(queryset, many=True)
-        serializer = self.get_serializer_class(request)
-        serializer = serializer(queryset, many=True)
+        serializer = ArticleListSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        queryset = Articles.objects.all()
+        queryset = self.get_queryset()
         article = get_object_or_404(queryset, pk=pk)
         # serializer = ArticleSerializer(article)
         serializer = self.get_serializer_class(request)
